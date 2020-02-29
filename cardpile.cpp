cccb311a4bffe109fb0f1d5de7a87c7cf4efaf10
Jason Tsorlinis <jasontsorlinis@192-168-1-105.tpgi.com.au>
2020-02-29 18:43:47 +1100

    switched fastrand to xorshift for fairness

--- cardpile.cpp
+++ cardpile.cpp
@@ -1,9 +1,15 @@
 #include "cardpile.h"
 
-#include <ctime>
-#include <random>
+#include <chrono>
 
-#include "QuickRand.h"
+unsigned int seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+
+unsigned int xorShift() {
+  seed ^= seed << 13;
+  seed ^= seed >> 17;
+  seed ^= seed << 5;
+  return seed;
+}
 
 CardPile::CardPile(const int num_of_decks) {
   for (auto x = 0; x < num_of_decks; x++) {
@@ -13,7 +19,6 @@ CardPile::CardPile(const int num_of_decks) {
   }
   m_original_cards_ = m_cards_;
   m_num_of_decks_ = num_of_decks;
-  fast_srand(static_cast<int>(time(nullptr)));
 }
 
 void CardPile::refresh() { m_cards_ = m_original_cards_; }
@@ -27,12 +32,9 @@ std::string CardPile::print() {
 }
 
 void CardPile::shuffle() {
-  // std::random_device rd;
-  // std::shuffle(mCards.begin(), mCards.end(), rd);
-
   // Fisher yates
   for (auto i = static_cast<int>(m_cards_.size()) - 1; i > 0; i--) {
-    const auto j = fast_rand() % (i + 1);
+    const auto j = xorShift() % (i + 1);
     std::swap(m_cards_[i], m_cards_[j]);
   }
 }
