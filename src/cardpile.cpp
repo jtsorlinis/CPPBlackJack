237d2a75c9aded04c29a2a8790ead6cdc1c7e734
Jason Tsorlinis <jtsorlinis@gmail.com>
2021-04-07 19:17:43 +1000

    Pcg (#2)
    * use pcg and nearly divisionless for shuffle
    
    * fixed range

--- src/cardpile.cpp
+++ src/cardpile.cpp
@@ -2,13 +2,35 @@
 
 #include <chrono>
 
-unsigned int seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+uint64_t state =
+    std::chrono::high_resolution_clock::now().time_since_epoch().count();
 
-unsigned int xorshift() {
-  seed ^= seed << 13;
-  seed ^= seed >> 17;
-  seed ^= seed << 5;
-  return seed;
+// From https://www.pcg-random.org/download.html#minimal-c-implementation
+uint32_t pcg32() {
+  uint64_t oldstate = state;
+  // Advance internal state
+  state = oldstate * 6364136223846793005ULL + 1;
+  // Calculate output function (XSH RR), uses old state for max ILP
+  uint32_t xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
+  uint32_t rot = oldstate >> 59u;
+  return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
+}
+
+// use nearly divisionless technique found here
+// https://github.com/lemire/FastShuffleExperiments
+uint32_t pcg32_range(uint32_t s) {
+  uint32_t x = pcg32();
+  uint64_t m = (uint64_t)x * (uint64_t)s;
+  uint32_t l = (uint32_t)m;
+  if (l < s) {
+    uint32_t t = -s % s;
+    while (l < t) {
+      x = pcg32();
+      m = (uint64_t)x * (uint64_t)s;
+      l = (uint32_t)m;
+    }
+  }
+  return m >> 32;
 }
 
 CardPile::CardPile(const int num_of_decks) {
@@ -34,7 +56,7 @@ std::string CardPile::print() {
 void CardPile::shuffle() {
   // Fisher yates
   for (auto i = static_cast<int>(m_cards_.size()) - 1; i > 0; i--) {
-    const auto j = xorshift() % (i + 1);
+    const auto j = pcg32_range(i + 1);
     std::swap(m_cards_[i], m_cards_[j]);
   }
 }
